93f1abef48cfc4790da95cdbb8c1b9ad06979c70
unknown <loicheung@vidarstudio.com>
2021-08-31 14:24:16 +0800

    aws demo

--- aws_google_facebook_signin_signup/lib/googlefacebook-signin.dart
+++ aws_google_facebook_signin_signup/lib/googlefacebook-signin.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 import 'package:flutter/cupertino.dart';
@@ -10,7 +11,6 @@ import 'package:http/http.dart' as http;
 import 'welcomescreen.dart';
 
 class WebViewGoogleFacebook extends StatefulWidget {
-
   String idendity_provider;
   WebViewGoogleFacebook(this.idendity_provider);
 
@@ -19,8 +19,7 @@ class WebViewGoogleFacebook extends StatefulWidget {
 }
 
 class WebViewGoogleFacebookState extends State<WebViewGoogleFacebook> {
-
-   final Completer<WebViewController> _webViewController =
+  final Completer<WebViewController> _webViewController =
       Completer<WebViewController>();
 
   @override
@@ -30,34 +29,28 @@ class WebViewGoogleFacebookState extends State<WebViewGoogleFacebook> {
     // if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
-
-
-
-
-  final COGNITO_CLIENT_ID = 'ADD CLIENT ID';
-  final COGNITO_Pool_ID = 'ADD POOL ID';
-  final COGNITO_POOL_URL = 'uniquedev.auth.us-east-2';  // CHANGE YOUR DOMAIN NAME
-  final CLIENT_SECRET = 'ADD SECRET KEY OF APP CLIENT';
-  var web_view_enable=0;
-  
-
- 
+  final COGNITO_CLIENT_ID = '60it31avpkfo28l8v4c0lruvfn';
+  final COGNITO_Pool_ID = 'ap-southeast-1_Zq3poZN55';
+  final COGNITO_POOL_URL =
+      'onesip.auth.ap-southeast-1'; // CHANGE YOUR DOMAIN NAME
+  final CLIENT_SECRET = 'd7punq2293gq3208okai0p19vkp1qrpkkeph0v02di5vakl6vhd';
+  var web_view_enable = 0;
 
   Widget getWebView() {
-    if(widget.idendity_provider=="Google")
-    {
-    widget.idendity_provider = "Google";
+    if (widget.idendity_provider == "Google") {
+      widget.idendity_provider = "Google";
+    } else {
+      widget.idendity_provider = "Facebook";
     }
-    else
-    {
-    widget.idendity_provider = "Facebook";
-    }
-    var signin=0;
+    var signin = 0;
 
     var url = "https://${COGNITO_POOL_URL}" +
-        ".amazoncognito.com/oauth2/authorize?identity_provider="+widget.idendity_provider+"&redirect_uri=" +
+        ".amazoncognito.com/oauth2/authorize?identity_provider=" +
+        widget.idendity_provider +
+        "&redirect_uri=" +
         "myapp://&response_type=CODE&client_id=${COGNITO_CLIENT_ID}" +
         "&scope=email%20openid%20profile%20aws.cognito.signin.user.admin";
+    print(url);
     return WebView(
       initialUrl: url,
       userAgent: 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) ' +
@@ -66,12 +59,11 @@ class WebViewGoogleFacebookState extends State<WebViewGoogleFacebook> {
       onWebViewCreated: (WebViewController webViewController) {
         _webViewController.complete(webViewController);
       },
-      
       navigationDelegate: (NavigationRequest request) {
-        if (request.url.startsWith("myapp://?code=") && signin==0) {
+        if (request.url.startsWith("myapp://?code=") && signin == 0) {
           String code = request.url.substring("myapp://?code=".length);
           signUserInWithAuthCode(code);
-          signin=1;
+          signin = 1;
           return NavigationDecision.prevent;
         }
 
@@ -81,18 +73,19 @@ class WebViewGoogleFacebookState extends State<WebViewGoogleFacebook> {
     );
   }
 
-
-
-    Future signUserInWithAuthCode(String authCode) async {
+  Future signUserInWithAuthCode(String authCode) async {
     String url = "https://${COGNITO_POOL_URL}" +
         ".amazoncognito.com/oauth2/token?grant_type=authorization_code&client_id=" +
         "${COGNITO_CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=" +
         authCode +
         "&redirect_uri=myapp://";
+    print("url = " + url);
     final response = await http.post(url,
         body: {},
         headers: {'Content-Type': 'application/x-www-form-urlencoded'});
+    print("statusCode = " + response.statusCode.toString());
     if (response.statusCode != 200) {
+      Navigator.pop(context);
       throw Exception("Received bad status code from Cognito for auth code:" +
           response.statusCode.toString() +
           "; body: " +
@@ -107,8 +100,7 @@ class WebViewGoogleFacebookState extends State<WebViewGoogleFacebook> {
     final session = new CognitoUserSession(idToken, accessToken,
         refreshToken: refreshToken);
 
-        final userPool =
-    new CognitoUserPool(COGNITO_Pool_ID,COGNITO_CLIENT_ID);
+    final userPool = new CognitoUserPool(COGNITO_Pool_ID, COGNITO_CLIENT_ID);
     final user = new CognitoUser(null, userPool, signInUserSession: session);
 
     // NOTE: in order to get the email from the list of user attributes, make sure you select email in the list of
@@ -120,14 +112,15 @@ class WebViewGoogleFacebookState extends State<WebViewGoogleFacebook> {
         break;
       }
     }
+    log("1 = " + tokenData['id_token'].toString());
+    log("2 = " + tokenData['access_token'].toString());
 
-print("login successfully.");
-Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => WelcomeScreen()),
-        );
-    
+    print("login successfully.");
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => WelcomeScreen()),
+    );
+
     return user;
   }
 
@@ -139,11 +132,7 @@ Navigator.push(
         },
         child: Scaffold(
             appBar: AppBar(
-                title: Text(widget.idendity_provider + " Authentication")
-            ),
-            body:  getWebView())
-    );
-    
-
-
-}}
+                title: Text(widget.idendity_provider + " Authentication")),
+            body: getWebView()));
+  }
+}
